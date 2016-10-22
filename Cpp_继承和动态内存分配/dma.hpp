e295938c12e9e62777e726049d71ae3722a1804b
carsonzhu <minzhu0702@outlook.com>
2016-10-22 15:35:46 +0800

    Create dma.hpp
    add dma.hpp

---
+++ Cpp_继承和动态内存分配/dma.hpp
@@ -0,0 +1,56 @@
+//
+//  dma.hpp
+//  继承与动态内存分配
+//
+//  Created by Min Zhu on 16/10/21.
+//
+//
+
+#ifndef dma_hpp
+#define dma_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+// Base Class Using DMA
+class baseDMA{
+private:
+    char * label;
+    int rating;
+    
+public:
+    baseDMA(const char * l = "null", int r = 0);
+    baseDMA(const baseDMA & rs);
+    virtual ~baseDMA();
+    baseDMA & operator=(const baseDMA & rs);
+    friend std::ostream & operator<<(std::ostream & os, const baseDMA & rs);
+};
+
+// derived class without DMA
+// no destructor needed
+// uses implicit copy constructor
+// uses implicit assignment operator
+class lacksDMA : public baseDMA{
+private:
+    enum{ COL_LEN = 40 };
+    char color[COL_LEN];
+public:
+    lacksDMA(const char * c = "blank", const char * l = "null", int r = 0);
+    lacksDMA(const char * c, const baseDMA & rs);
+    friend std::ostream & operator<<(std::ostream & os, const lacksDMA & rs);
+};
+
+// derived class with DMA
+class hasDMA :public baseDMA{
+private:
+    char * style;
+public:
+    hasDMA(const char * s = "none", const char * l = "null", int r = 0);
+    hasDMA(const char * s, const baseDMA & rs);
+    hasDMA(const hasDMA & hs);
+    ~hasDMA();
+    hasDMA & operator=(const hasDMA & rs);
+    friend std::ostream & operator<<(std::ostream & os, const hasDMA & rs);
+};
+
+#endif /* dma_hpp */
